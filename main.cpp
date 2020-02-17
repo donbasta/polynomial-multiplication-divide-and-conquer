2e3104651387b8eecca50e13f81f9b2f45ff9c15
donbasta <faddilafarras@gmail.com>
2020-02-17 22:12:10 +0700

    add main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,36 @@
 #include "Polinom.cpp"
+
+Polinom P;
+int degree;
+
+Polinom inputPolinomial(){
+	Polinom P;
+
+	cout << "Masukkan derajat polinom: ";
+	cin >> degree;
+	P = Polinom(degree);
+
+	for(int i=0; i<=P.size; i++){
+		cout << "Masukkan koefisien x^" << i << ": ";
+		cin >> P.koef[i];
+	}
+
+	return P;
+}
+
+int main(){
+
+	cout << "Masukkan derajat polinom: ";
+	cin >> degree;
+
+	P = inputPolinomial();
+	Q = inputPolinomial();
+
+	cout << P;
+
+
+	
+
+
+
+}
